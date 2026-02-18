c400f6e1ff4934726e0d6b7b064dc52296130a9f
JuhainaMT <juhaina@mvp-apps.ae>
2026-02-18 12:13:46 +0530

    fix: scroll fix

--- app/components/ExperiencesSlider.tsx
+++ app/components/ExperiencesSlider.tsx
@@ -122,45 +122,8 @@ export default function ExperiencesSlider() {
     };
   }, [isMobile, isReady]);
 
-  // Touch swipe for mobile
-  useEffect(() => {
-    if (!isMobile) return;
-
-    const slider = sliderRef.current;
-    if (!slider) return;
-
-    let startX = 0;
-    let scrollLeft = 0;
-    let isDragging = false;
-
-    const handleTouchStart = (e: TouchEvent) => {
-      isDragging = true;
-      startX = e.touches[0].pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
-    };
-
-    const handleTouchMove = (e: TouchEvent) => {
-      if (!isDragging) return;
-      e.preventDefault();
-      const x = e.touches[0].pageX - slider.offsetLeft;
-      const walk = (x - startX) * 2; // Scroll speed multiplier
-      slider.scrollLeft = scrollLeft - walk;
-    };
-
-    const handleTouchEnd = () => {
-      isDragging = false;
-    };
-
-    slider.addEventListener('touchstart', handleTouchStart, { passive: true });
-    slider.addEventListener('touchmove', handleTouchMove, { passive: false });
-    slider.addEventListener('touchend', handleTouchEnd);
-
-    return () => {
-      slider.removeEventListener('touchstart', handleTouchStart);
-      slider.removeEventListener('touchmove', handleTouchMove);
-      slider.removeEventListener('touchend', handleTouchEnd);
-    };
-  }, [isMobile]);
+  // Mobile: use wrapper ref for horizontal scroll (scroll container is the wrapper)
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
   return (
     <section className="experience-section" ref={sectionRef}>
@@ -197,8 +160,8 @@ export default function ExperiencesSlider() {
           </p>
         </div>
 
-        {/* SLIDER WRAPPER */}
-        <div className="experience-slider-wrapper">
+        {/* SLIDER WRAPPER - on mobile this div is the horizontal scroll container */}
+        <div className="experience-slider-wrapper" ref={wrapperRef}>
           <div
             className="experience-slider"
             ref={sliderRef}
